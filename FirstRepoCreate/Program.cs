2375933ca507888902423f78d441a96c541471b6
Atkare07 <96227615+Atkare07@users.noreply.github.com>
2024-10-13 14:07:57 +0530

    try create new branch and adddd

--- FirstRepoCreate/Program.cs
+++ FirstRepoCreate/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("try to create and push data through new branch");
         }
     }
 }
